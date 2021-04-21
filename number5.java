80b9a52088df3f00b0be1285c0a9e0822c8b25e0
justineAm <78581939+justineAm@users.noreply.github.com>
2021-04-21 10:22:07 +0800

    exercise number 5 with answer on  comment added

---
+++ number5.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise4;
+
+/**
+ *
+ * @author 2ndyrGroupA
+ */
+public class number5 {
+ //Number 5.
+//          Compare and contrast the classic for loop versus foreach. 
+//           What are the pros and cons of both sides?
+
+//To compare and contrast the classic for loop and the for each loop, 
+//For loops are appropriate loops when you know exactly how many times iteration 
+//you want in statements within the loop. For loop iterates a statement or a block 
+//of statements repeatedly until a specified expression evaluates to false.
+//While the Foreach loop is used to iterate through the items in object collections, 
+//List generic collections or array list collections. 
+//In case of Foreach the variable of the loop will be the same as the type of values under the array.
+// 
+//The advantage of using for loop is we can easily modify an array, 
+//we can easily get the array index within the loop. On the other hand 
+//he advantage of using foreach loop is  you can execute it in parallel by just using 
+//a parallel Stream instead of regular stream. It also allows you to filter and map elements. 
+//Once you are done with filtering and mapping, you can use forEach() to operate over them. 
+//        
+//And basically the main difference between for and for each loop is to set the number of iterations manually. 
+//In for loop you have to set the number of iteration manually while in for each loop the iteration will be 
+//counted automatically as per the length of an array.
+
+}
